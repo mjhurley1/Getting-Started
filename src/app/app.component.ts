a8949968b1181cacb264cccb7ba19df4aff8c467
Mike Hurley <mhurley@hurley-consulting.io>
2018-05-29 15:57:13 -0400

    second component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'pm root',
+  selector: 'pm-root',
   template: `
-  <div><h1>{{pageTitle}},/h1>
+  <div><h1>{{pageTitle}}</h1>
        <div>My First Component</div>
        </div>
        `
