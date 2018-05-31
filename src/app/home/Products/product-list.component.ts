4a66b2e444b9c511716bcea89c2232d7a225de39
Mike Hurley <mhurley@hurley-consulting.io>
2018-05-31 16:52:41 -0400

    moar comps

--- src/app/home/Products/product-list.component.ts
+++ src/app/home/Products/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'pm-products'
+    selector: 'pm-products',
     templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
