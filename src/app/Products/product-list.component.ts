71d1235b8b10790c57abd67f538dc068903ffd37
Mike Hurley <mhurley@hurley-consulting.io>
2018-06-03 12:34:00 -0400

    Input and Binding

--- src/app/Products/product-list.component.ts
+++ src/app/Products/product-list.component.ts
@@ -3,12 +3,14 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html'
+
 })
 export class ProductListComponent {
  pageTitle: string = 'Product List!';
  imageWidth: number = 50;
  imageMargin: number = 2;
  showImage: boolean = false; 
+ listFilter: string = 'cart';
  products: any[] = [
     {
         "productId": 1,
@@ -66,5 +68,5 @@ export class ProductListComponent {
     this.showImage = !this.showImage;
     }
  }
- 
+
 
